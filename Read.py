6fc7f6581c90956d66924d4ad25245c5a4008529
LeerLU <2801275800@qq.com>
2019-02-25 17:46:10 +0800

    Add files via upload
    数据前处理程式

---
+++ Read.py
@@ -0,0 +1,41 @@
+from snownlp import SnowNLP
+import pprint
+class Read(object):
+	"""docstring for Read"""
+	def __init__(self, arg):
+		super(Read, self).__init__()
+		self.arg = arg
+	@staticmethod	
+	def file_read_two(file_name,file_name_two,video_name,video_type):
+		datasetList = []
+		keywordsList = []
+		with open(file_name,encoding ='gbk') as file_obj,open(file_name_two,encoding = 'gbk') as file_obj2:
+			while True :
+				 content2 = file_obj2.readline()
+				 content = file_obj.readline()
+				 if not content :
+				 	break
+				 content2 = file_obj2.readline()
+				 content = file_obj.readline()
+				 time = content
+				 split = time.split(' --> ')
+				 start_time = split[0]
+				 end_time =split[1].strip('\n')
+				 #print(start_time)
+				 #print(end_time)
+				 content2 = file_obj2.readline()
+				 content = file_obj.readline()
+				 chinese = content.strip('\n')
+				 english = content2.strip('\n')
+				 feeling = SnowNLP(english)
+				 #print(chinese)
+				 content = file_obj.readline()
+				 content2 = file_obj2.readline()
+				 print(video_name,'+',start_time,'+',end_time,'+',english,'+',chinese,'+',video_type,'+',feeling.sentiments*100)
+				 d = dict(startTime = start_time,endTime = end_time,chinese_subtitle = chinese,english_subtitle = english,emotion = feeling.sentiments*100,videoName = video_name,videoType = video_type)
+				 datasetList.append(d)
+		return datasetList
+
+
+Read.file_read_two('F:\\data\\db1\\[Karen.Thompson.Walker.我们能从恐惧中学到什么].KarenThompsonWalker_2012G-480p.chs.srt','F:\\data\\db1\\[Karen.Thompson.Walker.我们能从恐惧中学到什么].KarenThompsonWalker_2012G-480p.eng.srt','我们能从恐惧中学到什么','演讲')
+input('enter a key out of the test')
